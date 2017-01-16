e9eb2d0ae37133b6629181079f6768504de5081f
Sven Wagener <sven.wagener@rheinschmiede.de>
2017-01-16 23:30:51 +0100

    Changed text on Launch request

--- play-podcast/index.php
+++ play-podcast/index.php
@@ -13,10 +13,10 @@ echo $output;
 function alexa_response( $input ) {
 	switch ( $input->request->type ) {
 		case "LaunchRequest":
-			$response = alexa_speak( "Dies ist Dein Launch Request" );
+			$response = alexa_speak( "Das ist das Podcast Universum. Starte es mit Alexa öffne 'Podcast Universe' und gebe Deinen Podcast Titel an. Leider kann ich nur bekannte Titel verstehen. Willst Du Deinen Podcast auch auf 'Podcast Universe' haben, schreibe uns eine kurze Email an meinpodcast@podcastuniverse.de mit Deinem gewünschtem Podcast. Wir fügen Deinen Podcast dann in Kürze in usere Liste ein." );
 			break;
 		case "SessionEndedRequest":
-			$response = alexa_speak( "Dies ist Dein Session ended Request." );
+			$response = alexa_speak( "Ende des Universums." );
 			break;
 		case "IntentRequest":
 			$response = alexa_speak( "Dies ist Dein Intent Request." );
